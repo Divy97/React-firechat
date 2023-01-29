22914d854fddb9898221e3bc28501c3684da0f2c
Divy <divyparekh1810@gmail.com>
2023-01-29 15:08:47 +0530

    bugs fixed

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ import { useAuthState, useDarkMode } from "./hooks";
 // Components
 import Channel from "./components/Channel";
 import Loader from "./components/Loader";
+import firebaseConfig from "./helpers/apiKeys";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBdPkvzwYY8nbgyvkW2eQxdnYnC232X8Cg",
-  authDomain: "chatapp-28137.firebaseapp.com",
-  projectId: "chatapp-28137",
-  storageBucket: "chatapp-28137.appspot.com",
-  messagingSenderId: "322129943319",
-  appId: "1:322129943319:web:42a007437422ad9fba7552",
-  measurementId: "G-QD7PMXTX1C",
-});
+firebase.initializeApp(firebaseConfig);
 
 const MoonIcon = (props) => (
   <svg
---
+++ src/helpers/apiKeys.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBdPkvzwYY8nbgyvkW2eQxdnYnC232X8Cg",
+  authDomain: "chatapp-28137.firebaseapp.com",
+  projectId: "chatapp-28137",
+  storageBucket: "chatapp-28137.appspot.com",
+  messagingSenderId: "322129943319",
+  appId: "1:322129943319:web:42a007437422ad9fba7552",
+  measurementId: "G-QD7PMXTX1C",
+};
+
+export default firebaseConfig;
